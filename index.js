bf2caa7d0adb54a8fed6f88b2ca9dce54e06fe96
sabagoginava <sabagoginava2@gmail.com>
2019-12-09 19:24:25 +0400

    lab7

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const axios = require('axios');
-
+var $ = require('jquery');
 fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then(response => response.json())
 .then(json => console.log(json))
-
-
 axios.get('https://jsonplaceholder.typicode.com/todos/1')
 .then((response) => {
   console.log(response.data);
@@ -12,3 +10,8 @@ axios.get('https://jsonplaceholder.typicode.com/todos/1')
 .catch(function (error) {
   console.log(error);
 });
+
+const HelloWorld = (x) => {
+  $('body').css('background','black');
+}
+HelloWorld();
